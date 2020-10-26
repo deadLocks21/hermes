84715f1b3e98dfe8f8c964dbbf2c40521d6fdfc4
deadLocks21 <45351098+deadLocks21@users.noreply.github.com>
2020-10-26 12:52:51 +0100

    Header of the plugin.

---
+++ hermes.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: H.E.R.M.E.S.
+Plugin URI: https://timothe.hofmann.fr/projects/hermes/
+Description: Plugin permettant un envoie de mail récurent à diférentes dates données avec des contenus différents.
+Author: Timothé HOFMANN
+Version: 1.0
+Author URI: http://timothe.hofmann.fr
+*/
