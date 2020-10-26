cdb74a50d5da8f774ceec411327d554f8e176c7f
deadLocks21 <45351098+deadLocks21@users.noreply.github.com>
2020-10-26 18:34:34 +0100

    Adds of a top level menu in the left.

--- hermes.php
+++ hermes.php
@@ -7,3 +7,35 @@ Author: Timothé HOFMANN
 Version: 1.0
 Author URI: http://timothe.hofmann.fr
 */
+
+add_action( 'admin_menu', 'hermes_options_page' );
+function hermes_options_page() {
+    add_menu_page(
+        'H.E.R.M.E.S.',
+        'H.E.R.M.E.S. Options',
+        'manage_options',
+        'hermes',
+        'hermes_options_page_html',
+        plugins_url('hermes/images/icon_logo.png')
+    );
+}
+
+function hermes_options_page_html() {
+    ?>
+    <div class="wrap">
+        <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+        <form action="options.php" method="post">
+            <?php
+            /*            // output security fields for the registered setting "wporg_options"
+                        settings_fields( 'wporg_options' );
+                        // output setting sections and their fields
+                        // (sections are registered for "wporg", each field is registered to a specific section)
+                        do_settings_sections( 'wporg' );
+                        // output save settings button
+                        submit_button( __( 'Save Settings', 'textdomain' ) );*/
+            ?>
+        </form>
+        <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+    </div>
+    <?php
+}
